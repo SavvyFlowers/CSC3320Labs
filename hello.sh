25ab888aaf253c35505044471cab4fb63286685f
Savannah <40171353+SavvyFlowers@users.noreply.github.com>
2021-05-01 21:32:48 -0400

    Add files via upload
    Lab 6 In-Lab Assignment

---
+++ hello.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#Savannah King 
+#sking86@student.gsu.edu
+echo "Welcome to computer science society!"
+date
+echo $PATH
+echo $USER
+echo $SHELL
+df
+echo "Please, could you loan me \$25.00?"
+echo "if x=2, x*x=4, x/2=1"
+ls [c]*.sh
+echo "Goodbye!"
+date +%I
